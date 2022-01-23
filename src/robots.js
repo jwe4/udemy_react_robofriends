b34b27602fd72a9266f4bfb90202745179d4506d
jwe4 <jeweaver@gmail.com>
2022-01-23 14:56:14 -0700

    save progress

---
+++ src/robots.js
@@ -0,0 +1,28 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere!april.biz',
+  },
+  {
+    id: 2,
+    name: 'Leanne Graham2',
+    username: 'Bret2',
+    email: 'Sincere2!april.biz',
+  },
+  {
+    id: 3,
+    name: 'Leanne Graham3',
+    username: 'Bret3',
+    email: 'Sincere3!april.biz',
+  },
+  {
+    id: 4,
+    name: 'Leanne Graham4',
+    username: 'Bret4',
+    email: 'Sincere4!april.biz',
+  },
+];
+
+export default robots;
