b4e368a8ba980204b1fc283d8b59f1ce83dda571
jwe4 <jeweaver@gmail.com>
2022-01-25 22:42:41 -0700

    saving progress

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../containers/CardList';
 import SearchBox from '../containers/SearchBox';
 import Scroll from '../containers/Scroll';
+import ErrorBoundry from './ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -37,7 +38,9 @@ class App extends Component {
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
--- src/containers/CardList.js
+++ src/containers/CardList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+  // if (true) {
+  //   throw new Error('baaaad');
+  // }
   return (
     <>
       {robots.map((user, i) => {
