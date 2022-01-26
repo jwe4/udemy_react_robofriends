c0acc5bc0fa21c453a411b0df971882480d255e5
jwe4 <jeweaver@gmail.com>
2022-01-26 09:16:58 -0700

    Updates

---
+++ precache-manifest.8c8bf0c9baae135976f2400d311f2a47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4a79c71886dff8e743c1d084e7ee40",
+    "url": "/udemy_react_robofriends/index.html"
+  },
+  {
+    "revision": "1b1a4133686499da29aa",
+    "url": "/udemy_react_robofriends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "09f975eae003494165b7",
+    "url": "/udemy_react_robofriends/static/css/main.dd22cb27.chunk.css"
+  },
+  {
+    "revision": "1b1a4133686499da29aa",
+    "url": "/udemy_react_robofriends/static/js/2.d679fc0b.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/udemy_react_robofriends/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "09f975eae003494165b7",
+    "url": "/udemy_react_robofriends/static/js/main.1c81f7c5.chunk.js"
+  },
+  {
+    "revision": "ca15dfce29514818126d",
+    "url": "/udemy_react_robofriends/static/js/runtime~main.0cdf513f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/udemy_react_robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
